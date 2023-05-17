9fbe06f87c3bc96ea8076e3c11119e7c5264cfbb
mrzogy <magbolmrzogy@gmail.com>
2023-05-17 17:12:47 +0300

    Initial Commit

---
+++ Zoom/ZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomApp.swift
+//  Zoom
+//
+//  Created by 3bood on 27/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
